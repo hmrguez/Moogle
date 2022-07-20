c54d2cb772b89683c38c0aa296f8afa731f6bc4d
vekt0R-HUB <d0t42f4n@gmail.com>
2022-07-19 22:16:42 -0400

    Update

--- MoogleEngine/Book.cs
+++ MoogleEngine/Book.cs
@@ -1,16 +1,39 @@
 namespace MoogleEngine;
 
-public class Book{
+public class Book
+{
     public Book(string title, string fullText, Dictionary<string, float> repetitions)
     {
         Title = title;
         FullText = fullText;
         Repetitions = repetitions;
-        SplittedText = FullText.Split(new char[]{' ',',',';',':'}, StringSplitOptions.RemoveEmptyEntries);
+        SplittedText = FullText.Split(new char[] { ' ', ',', ';', ':' }, StringSplitOptions.RemoveEmptyEntries);
     }
 
     public string Title { get; set; }
     public string FullText { get; set; }
-    public Dictionary<string,float> Repetitions { get; set; }
+    public Dictionary<string, float> Repetitions { get; set; }
     public string[] SplittedText { get; set; }
+
+    public IEnumerable<string> GetWords()
+    {
+        foreach (var item in SplittedText)
+        {
+            yield return item;
+        }
+    }
+}
+
+public static class Helper
+{
+    public static IEnumerable<string> Words(this IEnumerable<Book> books)
+    {
+        foreach (var item in books)
+        {
+            foreach (var item2 in item.GetWords())
+            {
+                yield return item2;
+            }
+        }
+    }
 }
--- MoogleEngine/Moogle.cs
+++ MoogleEngine/Moogle.cs
@@ -5,33 +5,103 @@ namespace MoogleEngine;
 
 public static class Moogle
 {
-    public static IEnumerable<Book> Books = Scan();
+    public static Book[] Books = Scan().ToArray();
+
+    public static IEnumerable<string> Words = Books.Words();
+
     public static SearchResult Query(string query)
     {
-        return new SearchResult(Search(query).ToArray(), " ");
+        SearchItem[] x = Search(query).ToArray();
+        if(x.Length==0) return new SearchResult(x,Suggestion(query));
+        else return new SearchResult(x,null!);
     }
+
+    public static string Suggestion(string query)
+    {
+        string[] x = query.Split(' ');
+        string temp = "";
+        foreach (var item in x)
+        {
+            if (Words.Contains(item)) temp += item + " ";
+            else
+            {
+                Dictionary<string, int> y = new();
+                bool cumple = false;
+                foreach (var item2 in Words)
+                {
+                    if(y.ContainsKey(item2)) continue;
+                    int f = LD(item, item2);
+                    if (f == 1)
+                    {
+                        temp += item2 + " ";
+                        cumple = true;
+                        break;
+                    }
+                    if (f < 3) y.Add(item2, f);
+                }
+                if(y.Count!=0)
+                if(!cumple) temp+=y.MinBy(x=>x.Value).Key + " ";
+            }
+        }
+        return temp;
+    }
+
+    public static int LD(string source1, string source2)
+    {
+        var source1Length = source1.Length;
+        var source2Length = source2.Length;
+
+        var matrix = new int[source1Length + 1, source2Length + 1];
+
+        if (source1Length == 0)
+            return source2Length;
+
+        if (source2Length == 0)
+            return source1Length;
+
+        for (var i = 0; i <= source1Length; matrix[i, 0] = i++) { }
+        for (var j = 0; j <= source2Length; matrix[0, j] = j++) { }
+
+        for (var i = 1; i <= source1Length; i++)
+        {
+            for (var j = 1; j <= source2Length; j++)
+            {
+                var cost = (source2[j - 1] == source1[i - 1]) ? 0 : 1;
+
+                matrix[i, j] = Math.Min(
+                    Math.Min(matrix[i - 1, j] + 1, matrix[i, j - 1] + 1),
+                    matrix[i - 1, j - 1] + cost);
+            }
+        }
+        return matrix[source1Length, source2Length];
+    }
+
+
+
+
+
     public static IEnumerable<SearchItem> Search(string query)
     {
         var x = query.Split(' ').ToList();
         foreach (var item in Books)
         {
+            if (x.Exists(p => !item.Repetitions.Keys.Contains(p))) continue;
 
-            if(x.Exists(p=>!item.Repetitions.Keys.Contains(p))) continue;
-
-            if (Score(item, query) != 0) yield return new SearchItem(item.Title, Snippet(item,query), Score(item, query));
+            if (Score(item, query) != 0) yield return new SearchItem(item.Title, Snippet(item, query), Score(item, query));
         }
     }
-    public static bool Exists<T>(int i, IEnumerable<T> a) => (i>=0 && i<a.Count());
-    public static string Snippet(Book book, string query){
+    public static bool Exists<T>(int i, IEnumerable<T> a) => (i >= 0 && i < a.Count());
+    public static string Snippet(Book book, string query)
+    {
         string[] words = query.Split(' ');
         string result = "";
         foreach (var item in words)
         {
             string temp = "";
-            int x = book.SplittedText.ToList().FindIndex(x=>x==item);
+            int x = book.SplittedText.ToList().FindIndex(x => x == item);
             for (int i = -5; i < 5; i++)
             {
-                if(Exists(x+i,book.SplittedText)) temp+= book.SplittedText[x+i] + " ";
+                if (Exists(x + i, book.SplittedText)) temp += book.SplittedText[x + i] + " ";
             }
             result += temp + " ... ";
         }
@@ -43,11 +113,11 @@ public static class Moogle
         float count = 0;
         foreach (var item in words)
         {
-            count += TF(book, item)*IDF(item);
+            count += TF(book, item) * IDF(item);
         }
         return count;
     }
-    public static float IDF(string word) => (float)Books.Count(x=>x.Repetitions.Keys.Contains(word))/Books.Count();
+    public static float IDF(string word) => (float)Books.Count(x => x.Repetitions.Keys.Contains(word)) / Books.Count();
     public static float TF(Book book, string word) => book.Repetitions.ContainsKey(word) ? book.Repetitions[word] / book.Repetitions.Values.Sum() : 0f;
     public static IEnumerable<Book> Scan()
     {
