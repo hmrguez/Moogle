71d6e14d2355f896c5085b7722c2e1dd2206b27d
vekt0R-HUB <d0t42f4n@gmail.com>
2022-12-31 00:20:00 -0500

    Scan and Trie Implementation

--- MoogleEngine/Moogle.cs
+++ MoogleEngine/Moogle.cs
@@ -1,13 +1,34 @@
-﻿using System.IO;
-using System;
-using System.Linq;
-namespace MoogleEngine;
+﻿namespace MoogleEngine;
 
 public static class Moogle
 {
+    public record Book(string Name, Trie Words);
+    
     public static SearchResult Query(string query)
     {
         throw new NotImplementedException();
     }
 
+    public static IEnumerable<Book> Scan()
+    {
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "../Content");
+        var files = Directory.GetFiles(path, "*.txt");
+
+        foreach (var file in files)
+        {
+            var trie = new Trie('^');
+            var sr = new StreamReader(file);
+            var text = sr.ReadToEnd();
+
+            var separators = text.Where(char.IsSeparator).ToArray();
+            var words = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var word in words)
+            {
+                trie.Insert(word);
+            }
+
+            yield return new Book(file, trie);
+        }
+    }
 }
---
+++ MoogleEngine/Trie.cs
@@ -0,0 +1,50 @@
+﻿namespace MoogleEngine;
+
+public class Trie
+{
+    public char Value { get; init; }
+    public Trie[] Children { get; init; }
+    public int Repetitions { get; set; }
+
+    public Trie(char value)
+    {
+        Value = value;
+        Children = new Trie[26];
+    }
+    
+    public (Trie, int) PrefixQuery(string word) => PrefixQuery(this, word);
+    private (Trie, int) PrefixQuery(Trie trie, string word)
+    {
+        var node = trie;
+        var c = 0;
+
+        while (c < word.Length && node?.Children[Utils.CharToIntParse(word[c])] != null)
+        {
+            node = node.Children[Utils.CharToIntParse(word[c])];
+            c++;
+        }
+        
+        return (node, c);
+    }
+
+    public void Insert(string word)
+    {
+        var pQuery = PrefixQuery(word);
+        pQuery.Item1.Insert(word,pQuery.Item2);
+    }
+    private void Insert(string word, int pos)
+    {
+        if (word.Length >= pos)
+        {
+            Repetitions++;
+            return;
+        }
+
+        var charToIntParse = Utils.CharToIntParse(word[pos]);
+        Children[charToIntParse] = new Trie(word[pos]);
+        
+        Children[charToIntParse].Insert(word,pos+1);
+    }
+
+    public bool Contains(string word) => PrefixQuery(word).Item2 == word.Length - 1;
+}
